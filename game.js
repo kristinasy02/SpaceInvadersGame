f511a5d7c1ad9f2bc23e88f775bc48470eed2ce8
kyordanova <ky@auxcode.com>
2023-02-09 20:41:49 +0200

    initialization of the game

--- game.js
+++ game.js
@@ -1,34 +1,49 @@
-    let container1 = document.getElementById('container1');
-    let container2 = document.getElementById('container2');
-    let container3 = document.getElementById('container3');
-    let container4 = document.getElementById('container4');
-    let container5 = document.getElementById('container5');
+let invadersArray = document.getElementById('invadersArray');
 
-    // Here you are on a good path!
-    // Convert this to two phases:
-    // Load them in one array, where every one has an CSS id # in the INITALISATION PART OF THE GAME
-    // Then display them in a second cycle in the ACTION part of the GAME LOOP
+// Here you are on a good path!
+// Convert this to two phases:
+// Load them in one array, where every one has an CSS id # in the INITALISATION PART OF THE GAME
+// Then display them in a second cycle in the ACTION part of the GAME LOOP
+let character1Array = [];
+let character2Array = [];
+let character2SecondArray = [];
+let character3SecondArray = [];
+let character3Array = [];
+for (i = 0; i < 11; i++) {
+    character1Array[i] = `<div class="character1"><img class="character1_spritesheet" src="img/spritesheet.png" alt="character1" width="1" height="1"><div>`;
+    character2Array[i] = `<div class="character2"><img class="character2_spritesheet" src="img/spritesheet.png" alt="character2" width="1" height="1"><div>`;
+    character2SecondArray[i] = `<div class="character2"><img class="character2_spritesheet" src="img/spritesheet.png" alt="character2" width="1" height="1"><div>`;
+    character3Array[i] = `<div class="character3"><img class="character3_spritesheet" src="img/spritesheet.png" alt="character3" width="1" height="1"><div>`;
+    character3SecondArray[i] = `<div class="character3"><img class="character3_spritesheet" src="img/spritesheet.png" alt="character3" width="1" height="1"><div>`;
 
-    for(i = 0; i < 10; i++) {
-        container1.innerHTML+=
-        `<div class="character1"><img class="character1_spritesheet" src="img/spritesheet.png" alt="character1" width="1" height="1"></div>`;
-        container2.innerHTML+=
-        `<div class="character2"><img class="character2_spritesheet" src="img/spritesheet.png" alt="character2" width="1" height="1"><div>`;
-        container3.innerHTML+=
-        `<div class="character2"><img class="character2_spritesheet" src="img/spritesheet.png" alt="character2" width="1" height="1"><div>`;
-        container4.innerHTML+=
-        `<div class="character3"><img class="character3_spritesheet" src="img/spritesheet.png" alt="character3" width="1" height="1"></div>`;
-        container5.innerHTML+=
-        `<div class="character3"><img class="character3_spritesheet" src="img/spritesheet.png" alt="character3" width="1" height="1"></div>`;
+}
+let allInvaders = [];
+allInvaders[0] = character1Array;
+allInvaders[1] = character2Array;
+allInvaders[2] = character2SecondArray;
+allInvaders[3] = character3Array;
+allInvaders[4] = character3SecondArray;
+let invadersBody = document.getElementById('invadersBody');
+let invaders  = document.getElementById('invaders');
+for (i = 0; i < allInvaders.length; i++) {
+    var row = document.createElement('tr');
+    row.classList.add('invadersRow');
+    for (j = 0; j < allInvaders[i].length; j++) {
+        var cell = document.createElement('cell');
+        cell.innerHTML = allInvaders[i][j];
+        row.appendChild(cell);
     }
+    invadersBody.appendChild(row);
+}
+invaders.append(invadersBody);
+
 
 var moveX = 0;
 const speed = 10;
 var w = document.body.clientWidth;
-console.log(w)
 
 if (document.getElementById('hero').style.left == 0) {
-    document.getElementById('hero').style.left = ((w/2) - 7.5) + "px"
+    document.getElementById('hero').style.left = ((w / 2) - 7.5) + "px"
 }
 
 window.addEventListener("keydown", Control);
@@ -38,23 +53,21 @@ function Control() {
     let hero = document.getElementById('hero')
     let position = document.getElementById('position')
 
-        if(key == "ArrowLeft") {
-            if (parseFloat (hero.style.left) < 260) return;
-            moveX = (parseFloat (hero.style.left) - speed);
-            hero.style.left = moveX + "px";
-            position.innerHTML = "X:" + moveX;
-            console.log(position.innerHTML)
-        }
-        else if (key == "ArrowRight") {
-            if (parseFloat (hero.style.left) > 830) return;
-            moveX = (parseFloat (hero.style.left) + speed);
-            hero.style.left = moveX + "px";
-            position.innerHTML = "X: " + moveX;
-        }
-        else
-        {
-            hero.style.left = parseFloat (hero.style.left) + "px";
-        }
+    if (key == "ArrowLeft") {
+        if (parseFloat(hero.style.left) < 260) return;
+        moveX = (parseFloat(hero.style.left) - speed);
+        hero.style.left = moveX + "px";
+        position.innerHTML = "X:" + moveX;
+    }
+    else if (key == "ArrowRight") {
+        if (parseFloat(hero.style.left) > 830) return;
+        moveX = (parseFloat(hero.style.left) + speed);
+        hero.style.left = moveX + "px";
+        position.innerHTML = "X: " + moveX;
+    }
+    else {
+        hero.style.left = parseFloat(hero.style.left) + "px";
+    }
 }
 
 // INITIALISATION
